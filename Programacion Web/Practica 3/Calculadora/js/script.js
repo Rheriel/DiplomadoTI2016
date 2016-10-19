ec0c816f927c71849cab27a0639dad8e9568ca4f
Jorge Abraham Pérez Gómez <rheriel@hotmail.co.jp>
2016-10-19 00:09:10 -0500

    Functions and methods
    Memory and bare-bones for functions.

--- Programacion Web/Practica 3/Calculadora/js/script.js
+++ Programacion Web/Practica 3/Calculadora/js/script.js
@@ -1,27 +1,43 @@
+var memory = 0;
+var currentNumber = 0;
+var lastNumber = 0;
+var newNumber = true;
+
 function sendButton(val) {
+
+	newNumber = false;
 	display = document.getElementById("display");
-	display.value += val;
+	if (display.value == "0"){
+		display.value = val;
+	} else {
+		display.value += val;
+	}
 }
 
 function sendOperator(val) {
-	if (val == "*") {
-		return multiply();
-	}
-	if (val == "/") {
-		return divide();
-	}
-	if (val == "+") {
-		return sum();
-	}
-	if (val == "-") {
-		return subtract();
-	}
+	
 	if (val == "+/-") {
 		return changeSymbol();
 	}
 	if (val == "=") {
 		return equal();
 	}
+
+	if (val == "*") {
+		lastOperator = multiply(); 
+	}
+	if (val == "/") {
+		lastOperator = divide();
+	}
+	if (val == "+") {
+		lastOperator = sum();
+	}
+	if (val == "-") {
+		lastOperator = subtract();
+	}
+	newNumber = false;
+	lastNumber = parseInt(document.getElementById("display").value);
+	lastOperator;
 }
 
 function sendCommand(val) {
@@ -29,7 +45,7 @@ function sendCommand(val) {
 		return memorySave();
 	}
 	if (val == "MR") {
-		return memoryReset();
+		return memoryRecall();
 	}
 	if (val == "M+") {
 		return addMemory();
@@ -46,45 +62,105 @@ function sendCommand(val) {
 }
 
 function multiply() {
+	console.log("Multiply.");
 
+	display = document.getElementById("display");
+	
+	if (newNumber) {
+		lastNumber = parseInt(display.value);
+		newNumber = false;
+	} else {
+		currentNumber *= lastNumber;
+	}
+
+	display.value = currentNumber;
 }
 
 function divide() {
+	console.log("Divide.");
+
+	display = document.getElementById("display");
 	
+	if (newNumber) {
+		lastNumber = parseInt(display.value);
+		newNumber = false;
+	} else {
+		currentNumber /= lastNumber;
+		newNumber = true;
+	}
+
+	display.value = currentNumber;
 }
 
 function sum() {
+	console.log("Sum.");
+
+	display = document.getElementById("display");
 	
+	if (newNumber) {
+		lastNumber = parseInt(display.value);
+		newNumber = false;
+	} else {
+		currentNumber += lastNumber;
+	}
+
+	display.value = currentNumber;
 }
 
 function subtract() {
+	console.log("Subtract.");
+
+	display = document.getElementById("display");
 	
+	if (newNumber) {
+		lastNumber = parseInt(display.value);
+		newNumber = false;
+	} else {
+		currentNumber -= lastNumber;
+		newNumber = true;
+	}
+
+	display.value = currentNumber;
+}
+
+function equal(){
+	if(lastOperator != "" && lastOperator != "+/-"){
+		lastOperator;
+	}
 }
 
 function changeSymbol() {
-	
+	display = document.getElementById("display");
+	display.value = parseInt(display.value) * -1;
 }
 
 function memorySave() {
-	
+	memory.push()
 }
 
-function memoryReset() {
-	
+function memoryRecall() {
+	display = document.getElementById("display");
+	display.value = memory;
 }
 
 function addMemory() {
-	
+	display = document.getElementById("display");
+	memory += parseInt(display.value);
 }
 
 function cls() {
-	
+	display = document.getElementById("display");
+	display.value = "0";
 }
 
 function memoryClear() {
-	
+	memory = 0;
 }
 
 function clearEverything() {
-	
+	display = document.getElementById("display");
+	display.value = "0";
+	currentNumber = 0;
+	newNumber = true;
+	memory = 0;
 }
